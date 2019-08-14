6716d6ee00a3f6fb95b6664a0c64a7accbbd65df
Lucas Soares Nogueira <lucasn@spi.local>
2019-08-14 15:31:45 -0300

    add count functions

--- teste.py
+++ teste.py
@@ -1,3 +1,14 @@
 def cria_conta(numero, titular, saldo, limite):
    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
    return conta
+
+def depositar(conta, valor): 
+    conta["saldo"] += valor
+
+def sacar(conta, valor):
+    conta['saldo'] -= valor
+
+def extrato(conta):
+    print("Saldo é {}".format(conta["saldo"]))
+
+    
