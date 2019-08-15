24bea8d1590ba62aea38f56e5d997f386c5a86ee
Lucas Soares Nogueira <lucasn@spi.local>
2019-08-15 15:59:43 -0300

    add Conta constructor

--- conta.py
+++ conta.py
@@ -1,4 +1,9 @@
 
 
 class Conta:
-    pass
+    def __init__(self):
+        print("Construindo objeto...{}".format(self))
+        self.numero = 123
+        self.titular = "Nico"
+        self.saldo = 55.0
+        self.limite = 1000.0
