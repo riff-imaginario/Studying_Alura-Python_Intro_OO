f6c8e7893491a0351dd1a947682426b49a41e540
Lucas Soares Nogueira <lucasn@spi.local>
2019-08-19 15:48:40 -0300

    add methods saca and deposita. set priv attributes

--- conta.py
+++ conta.py
@@ -3,7 +3,16 @@
 class Conta:
     def __init__(self):
         print("Construindo objeto...{}".format(self))
-        self.numero = 123
-        self.titular = "Nico"
-        self.saldo = 55.0
-        self.limite = 1000.0
+        self.__numero = 123
+        self.__titular = "Nico"
+        self.__saldo = 55.0
+        self.__limite = 1000.0
+
+def extrato(self):
+    print('Saldo de {} do titular {}'.format(self.saldo, self.titular))
+
+def deposita(self, valor):
+    self.saldo += valor
+
+def saca(self, valor):
+    self.saldo -= valor
