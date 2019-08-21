4046744823c75b7ff7edd037b1a57755b88b08eb
riff-imaginario <lucas.aleph.101@gmail.com>
2019-08-20 22:39:01 -0300

    fix extrato bug and identation

--- conta.py
+++ conta.py
@@ -8,33 +8,33 @@ class Conta:
         self.__saldo = saldo
         self.__limite = limite
 
-def extrato(self):
-    print('Saldo de {} do titular {}'.format(self.saldo, self.titular))
+    def extrato(self):
+        print('Saldo de {} do titular {}'.format(self.get_saldo(), self.get_titular()))
 
-def deposita(self, valor):
-    self.saldo += valor
+    def deposita(self, valor):
+        self.saldo += valor
 
-def saca(self, valor):
-    self.saldo -= valor
+    def saca(self, valor):
+        self.saldo -= valor
 
-def transfere(self, valor, destino):
-    self.saca(valor)  # Objeto conta origem
-    destino.saca(valor)  # Objeto conta destino
+    def transfere(self, valor, destino):
+        self.saca(valor)  # Objeto conta origem
+        destino.saca(valor)  # Objeto conta destino
 
-def get_saldo(self):
-    return self.__saldo
+    def get_saldo(self):
+        return self.__saldo
 
-def get_titular(self):
-    return self.__titular
+    def get_titular(self):
+        return self.__titular
 
-def get_limite(self):
-    return self.__limite
+    def get_limite(self):
+        return self.__limite
 
-def set_limite(self, limite):
-    self.__limite = limite
+    def set_limite(self, limite):
+        self.__limite = limite
 
-def set_titular(self, titular):
-    self.__titular = titular
+    def set_titular(self, titular):
+        self.__titular = titular
 
-def set_saldo(self, saldo):
-    self.__saldo = saldo
+    def set_saldo(self, saldo):
+        self.__saldo = saldo
